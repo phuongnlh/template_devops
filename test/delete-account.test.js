187533357ffec243eb3c151bd12c7a5e5200862f
Nguyễn Phương <phuongnlh.29@gmail.com>
2025-07-17 11:49:50 +0700

    Test complete

--- test/delete-account.test.js
+++ test/delete-account.test.js
@@ -2,4 +2,19 @@ const request = require("supertest");
 const { expect } = require("chai");
 const app = require("../src/app");
 
-describe("DELETE /accounts/:id", () => {});
+describe("DELETE /accounts/:id", () => {
+    it("should delete an existing account and return 204.", async () => {
+        const newAcc = await request(app)
+            .post("/accounts")
+            .send({ name: "Harry Potter", email: "hp@gmail.com" });
+        const id = newAcc.body.id;
+
+        const res = await request(app).delete(`/accounts/${id}`);
+        expect(res.status).to.equal(204);
+    });
+
+    it("should return 404 if deleting a non-existing account.", async () => {
+        const res = await request(app).delete("/accounts/9999");
+        expect(res.status).to.equal(404);
+    });
+});
--- test/get-account.test.js
+++ test/get-account.test.js
@@ -2,4 +2,17 @@ const request = require("supertest");
 const { expect } = require("chai");
 const app = require("../src/app");
 
-describe("GET /accounts/:id", () => {});
+describe("GET /accounts/:id", () => {
+  it("should return an existing account by ID.", async () => {
+    const newAcc = await request(app)
+      .post("/accounts")
+      .send({ name: "Jose Biden", email: "jb@gmail.com" });
+    const id = newAcc.body.id;
+
+    const res = await request(app).get(`/accounts/${id}`);
+    expect(res.status).to.equal(200);
+    expect(res.body.id).to.equal(id);
+    expect(res.body.name).to.equal(newAcc.body.name);
+    expect(res.body.email).to.equal(newAcc.body.email);
+  });
+});
